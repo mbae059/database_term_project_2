3994e7e3d2a6b0777f03ddf05900688d54788c3f
mbae059 <mbae059@gmail.com>
2023-11-28 21:12:22 +0900

    updated sql. This works in dbeaver.
    Warning!
    
    This works only when the database is created.
    The database itself will have to be created on its own.

--- DDL_query.sql
+++ DDL_query.sql
@@ -1,6 +1,3 @@
-create database db_trade; 
-use db_trade; 
-
 CREATE TABLE Player (
     player_id INT PRIMARY KEY,
     player_name VARCHAR(10),
@@ -11,17 +8,15 @@ CREATE TABLE Player (
     birth VARCHAR(15),
     income INT,
     agent_id INT,
-    phone_number VARCHAR(20),
-    FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE SET NULL
+    phone_number VARCHAR(20)
 );
+
 CREATE TABLE Player_Record (
     record_id INT PRIMARY KEY,
     player_id INT,
     start_date VARCHAR(15),
     end_date VARCHAR(15),
-    team_id INT,
-    FOREIGN KEY (player_id) REFERENCES Player(player_id) ON DELETE NO ACTION,
-    FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE NO ACTION
+    team_id INT  
 );
 CREATE TABLE Agent (
     agent_id INT PRIMARY KEY,
@@ -33,33 +28,29 @@ CREATE TABLE Owner (
     owner_id INT PRIMARY KEY,
     team_id INT,
     owner_name VARCHAR(10),
-    owner_age INT,
-FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE CASCADE
+    owner_age INT
 );
 CREATE TABLE Team (
     team_id INT PRIMARY KEY,
     team_name VARCHAR(10),
     owner_id INT,
     director_id INT,
-    establishment_year INT,
-    FOREIGN KEY (owner_id) REFERENCES Owner(owner_id) ON DELETE SET NULL,
-    FOREIGN KEY (director_id) REFERENCES Director(director_id) ON DELETE SET NULL
+    establishment_year INT
 );
 CREATE TABLE Director (
     director_id INT PRIMARY KEY,
     director_name VARCHAR(10),
     director_year INT,
     team_id INT,
-    income INT,
-    FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE SET NULL
+    income INT
 );
-CREATE TABLE User (
-    user_id INT PRIMARY KEY,
-    user_name VARCHAR(20),
-    user_password VARCHAR(25),
+
+CREATE TABLE Client (
+    Client_id INT PRIMARY KEY,
+    Client_name VARCHAR(20),
+    Client_password VARCHAR(25),
     team_id INT,
-    user_type VARCHAR(10),
-    FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE SET NULL
+    Client_type VARCHAR(10)
 );
 CREATE TABLE Player_Agent (
     player_id INT,
@@ -67,23 +58,17 @@ CREATE TABLE Player_Agent (
     team_id INT,
     contract_date VARCHAR(15),
     contract_term INT,
-    contract_payment INT,
-    FOREIGN KEY (player_id) REFERENCES Player(player_id) ON DELETE NO ACTION,
-    FOREIGN KEY (agent_id) REFERENCES Agent(agent_id) ON DELETE NO ACTION
+    contract_payment INT
 );
 CREATE TABLE Team_Award (
     awards_id INT,
     year INT,
-    team_id INT,
-    FOREIGN KEY (awards_id) REFERENCES Awards(awards_id) ON DELETE CASCADE,
-    FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE CASCADE
+    team_id INT
 );
 CREATE TABLE Individual_Award (
     awards_id INT,
     year INT,
-    player_id INT,
-    FOREIGN KEY (awards_id) REFERENCES Awards(awards_id) ON DELETE CASCADE,
-    FOREIGN KEY (player_id) REFERENCES Player(player_id) ON DELETE CASCADE
+    player_id INT
 );
 CREATE TABLE Awards (
     awards_id INT PRIMARY KEY,
@@ -94,10 +79,7 @@ CREATE TABLE Baseball_Records (
     team1_id INT,
     team2_id INT,
     baseball_stadium_id INT,
-    score VARCHAR(10),
-    FOREIGN KEY (team1_id) REFERENCES Team(team_id) ON DELETE CASCADE,
-    FOREIGN KEY (team2_id) REFERENCES Team(team_id) ON DELETE CASCADE,
-    FOREIGN KEY (baseball_stadium_id) REFERENCES Baseball_Stadium(baseball_stadium_id) ON DELETE NO ACTION
+    score VARCHAR(10)
 );
 CREATE TABLE Baseball_Stadium (
     baseball_stadium_id INT PRIMARY KEY,
@@ -105,3 +87,57 @@ CREATE TABLE Baseball_Stadium (
     area VARCHAR(20),
     capacity INT
 );
+
+ALTER TABLE Player
+ADD CONSTRAINT fk_Player_reference_Team
+FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE SET NULL;
+
+ALTER TABLE Player_Record
+ADD CONSTRAINT fk_Player_Record_reference_Player
+FOREIGN KEY (player_id) REFERENCES Player(player_id) ON DELETE NO action,
+ADD CONSTRAINT fk_Player_Record_reference_Team
+FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE no action;
+
+ALTER TABLE Owner
+ADD CONSTRAINT fk_Player_Record_reference_Team
+FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE CASCADE;
+
+ALTER TABLE Team
+ADD CONSTRAINT fk_Team_reference_Owner
+FOREIGN KEY (owner_id) REFERENCES Owner(owner_id) ON DELETE SET NULL,
+ADD CONSTRAINT fk_Team_reference_Director
+FOREIGN KEY (director_id) REFERENCES Director(director_id) ON DELETE SET NULL;
+
+ALTER TABLE Director
+ADD CONSTRAINT fk_Director_reference_Team
+FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE SET NULL;
+
+ALTER TABLE Client
+ADD CONSTRAINT fk_Client_reference_Team
+FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE SET NULL;
+
+ALTER TABLE Player_Agent
+ADD CONSTRAINT fk_Player_Agent_reference_Player
+FOREIGN KEY (player_id) REFERENCES Player(player_id) ON DELETE NO ACTION,
+ADD CONSTRAINT fk_Player_Agent_reference_Agent
+FOREIGN KEY (agent_id) REFERENCES Agent(agent_id) ON DELETE NO ACTION;
+
+ALTER TABLE Team_Award
+ADD CONSTRAINT fk_Team_Award_reference_Award
+FOREIGN KEY (awards_id) REFERENCES Awards(awards_id) ON DELETE CASCADE,
+ADD CONSTRAINT fk_Team_Award_reference_Team
+FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE CASCADE;
+
+ALTER TABLE Individual_Award
+ADD CONSTRAINT fk_Individual_Award_reference_Award
+FOREIGN KEY (awards_id) REFERENCES Awards(awards_id) ON DELETE CASCADE,
+ADD CONSTRAINT fk_Individual_Award_reference_Player
+FOREIGN KEY (player_id) REFERENCES Player(player_id) ON DELETE cascade;
+
+ALTER TABLE Baseball_Records
+ADD CONSTRAINT fk_Baseball_Records_reference_Team1
+FOREIGN KEY (team1_id) REFERENCES Team(team_id) ON DELETE cascade,
+ADD CONSTRAINT fk_Baseball_Records_reference_Team2
+FOREIGN KEY (team2_id) REFERENCES Team(team_id) ON DELETE cascade,
+ADD CONSTRAINT fk_Baseball_Records_reference_Baseball_Stadium
+FOREIGN KEY (baseball_stadium_id) REFERENCES Baseball_Stadium(baseball_stadium_id) ON DELETE NO ACTION;
