f4a202d7b0c6a6cee863e8effd6b775dcebeb75d
mbae059 <mbae059@gmail.com>
2023-11-29 23:11:05 +0900

    complete player generator

--- create_dummy_data/Player.py
+++ create_dummy_data/Player.py
@@ -9,16 +9,21 @@ cnt = 0
 start_date = datetime(1990, 1, 1)
 end_date = datetime(2003, 12, 31)
 
+
 #TODO insert uniform_number
 for i in range(1, 11):
     j = (i-1) * 10 + 1
-    file.write(f"insert into player values ('player#{j+0}', 'team#{i}', 'i', 'P', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '1')\n")
-    file.write(f"insert into player values ('player#{j+1}', 'team#{i}', 'i', 'C', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '2')\n")
-    file.write(f"insert into player values ('player#{j+2}', 'team#{i}', 'i', '1B', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '3')\n")
-    file.write(f"insert into player values ('player#{j+3}', 'team#{i}', 'i', '2B', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '4')\n")
-    file.write(f"insert into player values ('player#{j+4}', 'team#{i}', 'i', 'SS', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '5')\n")
-    file.write(f"insert into player values ('player#{j+5}', 'team#{i}', 'i', '3B', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '6')\n")
-    file.write(f"insert into player values ('player#{j+6}', 'team#{i}', 'i', 'LF', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '7')\n")
-    file.write(f"insert into player values ('player#{j+7}', 'team#{i}', 'i', 'CF', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '8')\n")
-    file.write(f"insert into player values ('player#{j+8}', 'team#{i}', 'i', 'RF', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '9')\n")
-    file.write(f"insert into player values ('player#{j+9}', 'team#{i}', 'i', 'DH', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '10')\n")
+    uniform_number = set()
+    while len(uniform_number)<10:
+        uniform_number.add(random.randint(1,99))
+    li = list(uniform_number)
+    file.write(f"insert into player values ('player#{j+0}', 'team#{i}', '{i}', 'P', '{li[0]}', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '1')\n")
+    file.write(f"insert into player values ('player#{j+1}', 'team#{i}', '{i}','C', '{li[1]}', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '2')\n")
+    file.write(f"insert into player values ('player#{j+2}', 'team#{i}', '{i}', '1B', '{li[2]}', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '3')\n")
+    file.write(f"insert into player values ('player#{j+3}', 'team#{i}', '{i}', '2B', '{li[3]}', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '4')\n")
+    file.write(f"insert into player values ('player#{j+4}', 'team#{i}', '{i}', 'SS', '{li[4]}', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '5')\n")
+    file.write(f"insert into player values ('player#{j+5}', 'team#{i}', '{i}', '3B', '{li[5]}', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '6')\n")
+    file.write(f"insert into player values ('player#{j+6}', 'team#{i}', '{i}', 'LF', '{li[6]}', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '7')\n")
+    file.write(f"insert into player values ('player#{j+7}', 'team#{i}', '{i}', 'CF', '{li[7]}', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '8')\n")
+    file.write(f"insert into player values ('player#{j+8}', 'team#{i}', '{i}', 'RF', '{li[8]}', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '9')\n")
+    file.write(f"insert into player values ('player#{j+9}', 'team#{i}', '{i}', 'DH', '{li[9]}', '{generate_random_date(start_date, end_date)}', '{random.randint(1, 100)}', '10')\n")
