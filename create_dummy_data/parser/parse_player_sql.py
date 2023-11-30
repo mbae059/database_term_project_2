2e73c257b8c7ba108a471a9c06744953e2fda234
mbae059 <mbae059@gmail.com>
2023-11-30 23:40:51 +0900

    player parsing in one code

---
+++ create_dummy_data/parser/parse_player_sql.py
@@ -0,0 +1,59 @@
+import re
+from datetime import datetime
+
+def parse_list(query):
+    values_match = re.search(r"values \((.*?)\)", query)
+    if values_match:
+        values_str = values_match.group(1)
+        values_list = [value.strip("'") for value in values_str.split(",")]
+        values_list = [value.strip().lstrip("'") for value in values_list]
+        return values_list
+    else:
+        return None
+def parse_from_player_sql(player_name, file_path=None):
+    if file_path is None:
+        file_path = '../sql/player.sql'
+    file = open(file_path, 'r')
+    for line in file:
+        if player_name in line:
+            query = line.strip()
+            return parse_list(query)
+    return None
+
+def parse_player_id_from_player_sql(list_player_sql):
+    return int(list_player_sql[0])
+
+def parse_team_name_from_player_sql(list_player_sql):
+    return list_player_sql[2]
+
+def parse_team_id_from_player_sql(list_player_sql):
+    return int(list_player_sql[3])
+
+def parse_position_from_player_sql(list_player_sql):
+    return list_player_sql[4]
+def parse_uniform_number_from_player_sql(list_player_sql):
+    return int(list_player_sql[5])
+def parse_birth_from_player_sql(list_player_sql):
+    date_string = list_player_sql[6]
+    date_object = datetime.strptime(date_string, '%Y-%m-%d').date()
+    return date_object
+def parse_income_from_player_sql(list_player_sql):
+    return int(list_player_sql[7])
+def parse_agent_id_from_player_sql(list_player_sql):
+    return int(list_player_sql[8])
+
+# Extracting the date string from the query
+# Parsing the date string
+
+if __name__ == "__main__":
+    insert_query = "insert into player values ('77', 'player#77', 'team#8', '8', 'LF', '81', '1994-07-26', '28', '7')"
+    player_name = 'player#76'
+    li = parse_from_player_sql(player_name)
+    print(li)
+    print(parse_team_name_from_player_sql(li))
+    print(parse_team_id_from_player_sql(li))
+    print(parse_position_from_player_sql(li))
+    print(parse_uniform_number_from_player_sql(li))
+    print(parse_birth_from_player_sql(li))
+    print(parse_income_from_player_sql(li))
+    print(parse_agent_id_from_player_sql(li))
