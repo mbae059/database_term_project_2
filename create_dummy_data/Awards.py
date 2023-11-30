409becb90e811f84aef14abc61fcd14e1468476a
mbae059 <mbae059@gmail.com>
2023-11-30 23:50:18 +0900

    add primary key

--- create_dummy_data/Awards.py
+++ create_dummy_data/Awards.py
@@ -1,4 +1,10 @@
 file = open('sql/awards.sql', 'w')
 
+cnt = 1
 for i in range(1, 101):
-    file.write(f"insert into awards values ('award_#{i}')\n")
+    file.write(f"insert into awards values ('{cnt}', 'team_award_#{i}')\n")
+    cnt += 1
+
+for i in range(1, 101):
+    file.write(f"insert into awards values ('{cnt}', 'individual_award_#{i}')\n")
+    cnt += 1
