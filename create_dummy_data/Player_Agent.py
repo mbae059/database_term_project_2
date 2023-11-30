a21170b7f6ee4ec3f23b8c3bd8320649fb61461d
mbae059 <mbae059@gmail.com>
2023-11-30 23:41:30 +0900

    create player_agent generator

--- create_dummy_data/Player_Agent.py
+++ create_dummy_data/Player_Agent.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import random
 from create_dummy_data.generate_raw.generate_random_date import generate_random_date
-
+from create_dummy_data.parser.parse_player_sql import parse_team_id_from_player_sql, parse_player_id_from_player_sql, parse_birth_from_player_sql, parse_from_player_sql
 file = open('sql/player_agent.sql', 'w')
 
 start_date = datetime(2018, 1, 1)
@@ -9,10 +9,20 @@ end_date = datetime(2023, 12, 1)
 
 
 for i in range(1,300):
-    team_id = random.randint(1,10)
-    player_id = random.randint(team_id*10-9, team_id*10)
-    sd = generate_random_date(start_date, end_date)
-    ed = generate_random_date(start_date, end_date)
-    if sd > ed :
-        sd, ed = ed, sd
-    file.write(f"insert into player_record values ('{i}', '{player_id}', '{sd}', '{ed}', '{team_id}')\n")
+    file_path = 'sql/player.sql'
+    player_name = 'player#' + str(random.randint(1,100))
+    query = parse_from_player_sql(player_name, file_path)
+    team_id = parse_team_id_from_player_sql(query)
+    player_id = parse_player_id_from_player_sql(query)
+
+    player_birth = parse_birth_from_player_sql(query)
+    player_birth_plus_15_years = player_birth + timedelta(days=365 * 15)
+
+    date = []
+    for _ in range(6):
+        date.append(generate_random_date(player_birth_plus_15_years, datetime.now().date()))
+    date = sorted(date)
+    file.write(f"insert into player_record values ('{i}', '{player_id}', '{date[0]}', '{date[1]}', '{team_id}')\n")
+    file.write(f"insert into player_record values ('{i}', '{player_id}', '{date[2]}', '{date[3]}', '{team_id}')\n")
+    file.write(f"insert into player_record values ('{i}', '{player_id}', '{date[4]}', '{date[5]}', '{team_id}')\n")
+    file.write(f"insert into player_record values ('{i}', '{player_id}', '{date[4]}', '{date[5]}', '{team_id}')\n")
