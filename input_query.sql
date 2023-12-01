a3646130665e46728c58458a05cb2bff0f4a7c2d
mbae059 <mbae059@gmail.com>
2023-12-01 14:28:40 +0900

    not needed

--- input_query.sql
+++ input_query.sql
@@ -10,6 +10,100 @@ drop table if exists Individual_Award;
 drop table if exists Awards;
 drop table if exists Baseball_Records;
 drop table if exists Baseball_Stadium;
+CREATE TABLE Player (
+    player_id SERIAL PRIMARY KEY,
+    player_name VARCHAR(10),
+    team_name VARCHAR(10),
+    team_id INT,
+    position VARCHAR(10),
+    uniform_number INT,
+    birth DATE,
+    income INT,
+    agent_id INT
+);
+
+CREATE TABLE Player_Record (
+    record_id SERIAL PRIMARY KEY,
+    player_id INT,
+    start_date DATE,
+    end_date DATE,
+    team_id INT  
+);
+CREATE TABLE Agent (
+    agent_id SERIAL PRIMARY KEY,
+    agent_name VARCHAR(25),
+    age INT,
+    contact_info VARCHAR(20)
+);
+CREATE TABLE Owner (
+    owner_id SERIAL PRIMARY KEY,
+    team_id INT,
+    owner_name VARCHAR(25),
+    owner_age INT,
+    budget INT
+);
+CREATE TABLE Team (
+    team_id SERIAL PRIMARY KEY,
+    team_name VARCHAR(25),
+    owner_id INT,
+    director_id INT,
+    establishment_year INT
+);
+CREATE TABLE Director (
+    director_id SERIAL PRIMARY KEY,
+    director_name VARCHAR(25),
+    director_year INT,
+    team_id INT,
+    income INT
+);
+
+CREATE TABLE Client (
+    client_id SERIAL PRIMARY KEY,
+    client_name VARCHAR(20),
+    client_password VARCHAR(25),
+    team_id INT,
+    client_type VARCHAR(20)
+);
+CREATE TABLE Player_Agent (
+    player_agent_id SERIAL PRIMARY KEY,
+    player_id INT,
+    agent_id INT,
+    team_id INT,
+    contract_date DATE,
+    contract_term INT,
+    contract_payment INT
+);
+CREATE TABLE Team_Award (
+    team_award_id SERIAL PRIMARY KEY,
+    awards_id INT,
+    year INT,
+    team_id INT
+);
+CREATE TABLE Individual_Award (
+    individual_award_id SERIAL PRIMARY KEY,
+    awards_id INT,
+    year INT,
+    player_id INT
+);
+CREATE TABLE Awards (
+    awards_id SERIAL PRIMARY KEY,
+    awards_name VARCHAR(255)
+);
+CREATE TABLE Baseball_Records (
+    baseball_records_id SERIAL PRIMARY KEY,
+    team1_id INT,
+    team2_id INT,
+    baseball_stadium_id INT,
+    score VARCHAR(10)
+);
+CREATE TABLE Baseball_Stadium (
+    baseball_stadium_id SERIAL PRIMARY KEY,
+    location VARCHAR(255),
+    area INT,
+    capacity INT
+);
+
+
 insert into agent (agent_name, age, contact_info) values ('gcXFF', 27, '010-9629-9654');
 insert into agent (agent_name, age, contact_info) values ('jtRoYkY3', 55, '010-9507-9428');
 insert into agent (agent_name, age, contact_info) values ('K2AHyInhZ', 55, '010-1856-0453');
@@ -2894,3 +2988,56 @@ insert into team_award (awards_id, year, team_id) values ('65', 2023, 2);
 insert into team_award (awards_id, year, team_id) values ('40', 2020, 2);
 insert into team_award (awards_id, year, team_id) values ('19', 2017, 9);
 
+ALTER TABLE Player
+ADD CONSTRAINT fk_Player_reference_Team
+FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE SET NULL;
+
+ALTER TABLE Player_Record
+ADD CONSTRAINT fk_Player_Record_reference_Player
+FOREIGN KEY (player_id) REFERENCES Player(player_id) ON DELETE NO action,
+ADD CONSTRAINT fk_Player_Record_reference_Team
+FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE no action;
+
+ALTER TABLE Owner
+ADD CONSTRAINT fk_Player_Record_reference_Team
+FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE CASCADE;
+
+ALTER TABLE Team
+ADD CONSTRAINT fk_Team_reference_Owner
+FOREIGN KEY (owner_id) REFERENCES Owner(owner_id) ON DELETE SET NULL,
+ADD CONSTRAINT fk_Team_reference_Director
+FOREIGN KEY (director_id) REFERENCES Director(director_id) ON DELETE SET NULL;
+
+ALTER TABLE Director
+ADD CONSTRAINT fk_Director_reference_Team
+FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE SET NULL;
+
+ALTER TABLE Client
+ADD CONSTRAINT fk_Client_reference_Team
+FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE SET NULL;
+
+ALTER TABLE Player_Agent
+ADD CONSTRAINT fk_Player_Agent_reference_Player
+FOREIGN KEY (player_id) REFERENCES Player(player_id) ON DELETE NO ACTION,
+ADD CONSTRAINT fk_Player_Agent_reference_Agent
+FOREIGN KEY (agent_id) REFERENCES Agent(agent_id) ON DELETE NO ACTION;
+
+ALTER TABLE Team_Award
+ADD CONSTRAINT fk_Team_Award_reference_Award
+FOREIGN KEY (awards_id) REFERENCES Awards(awards_id) ON DELETE CASCADE,
+ADD CONSTRAINT fk_Team_Award_reference_Team
+FOREIGN KEY (team_id) REFERENCES Team(team_id) ON DELETE CASCADE;
+
+ALTER TABLE Individual_Award
+ADD CONSTRAINT fk_Individual_Award_reference_Award
+FOREIGN KEY (awards_id) REFERENCES Awards(awards_id) ON DELETE CASCADE,
+ADD CONSTRAINT fk_Individual_Award_reference_Player
+FOREIGN KEY (player_id) REFERENCES Player(player_id) ON DELETE cascade;
+
+ALTER TABLE Baseball_Records
+ADD CONSTRAINT fk_Baseball_Records_reference_Team1
+FOREIGN KEY (team1_id) REFERENCES Team(team_id) ON DELETE cascade,
+ADD CONSTRAINT fk_Baseball_Records_reference_Team2
+FOREIGN KEY (team2_id) REFERENCES Team(team_id) ON DELETE cascade,
+ADD CONSTRAINT fk_Baseball_Records_reference_Baseball_Stadium
+FOREIGN KEY (baseball_stadium_id) REFERENCES Baseball_Stadium(baseball_stadium_id) ON DELETE NO ACTION;
