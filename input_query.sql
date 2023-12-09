11b96044546baa87c35ea640a0add7839e05b941
mbae059 <mbae059@gmail.com>
2023-12-10 05:12:39 +0900

    change create table query

--- input_query.sql
+++ input_query.sql
@@ -1,109 +1,92 @@
-drop table if exists Player;
-drop table if exists Agent;
-drop table if exists Owner;
-drop table if exists Team;
-drop table if exists Director;
-drop table if exists Client;
-drop table if exists Player_Agent;
-drop table if exists Team_Award;
-drop table if exists Individual_Award;
-drop table if exists Awards;
-drop table if exists Baseball_Records;
-drop table if exists Baseball_Stadium;
+drop table if exists player;
+drop table if exists team;
+drop table if exists belongs_to;
+drop table if exists owner;
+drop table if exists owns;
+drop table if exists director;
+drop table if exists directs;
+drop table if exists awards;
+drop table if exists player_won;
+drop table if exists team_won;
+drop table if exists client;
 
 -- SERIAL
 -- auto increment int
-CREATE TABLE Player (
-    player_id SERIAL PRIMARY KEY,
-    player_name VARCHAR(10),
-    team_name VARCHAR(10),
-    team_id INT,
-    position VARCHAR(10),
-    uniform_number INT,
-    birth DATE,
-    income INT,
-    agent_id INT
+CREATE TABLE player (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(63),
+    position VARCHAR(63),
+    birth DATE
 );
 
-CREATE TABLE Player_Record (
-    record_id SERIAL PRIMARY KEY,
-    player_id INT,
-    start_date DATE,
-    end_date DATE,
-    team_id INT  
-);
-CREATE TABLE Agent (
-    agent_id SERIAL PRIMARY KEY,
-    agent_name VARCHAR(25),
-    age INT,
-    contact_info VARCHAR(20)
-);
-CREATE TABLE Owner (
-    owner_id SERIAL PRIMARY KEY,
-    team_id INT,
-    owner_name VARCHAR(25),
-    owner_age INT,
-    budget INT
-);
-CREATE TABLE Team (
-    team_id SERIAL PRIMARY KEY,
-    team_name VARCHAR(25),
-    owner_id INT,
-    director_id INT,
+CREATE TABLE team (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(25),
     establishment_year INT
 );
-CREATE TABLE Director (
-    director_id SERIAL PRIMARY KEY,
-    director_name VARCHAR(25),
-    director_year INT,
-    team_id INT,
-    income INT
-);
 
-CREATE TABLE Client (
-    client_id SERIAL PRIMARY KEY,
-    client_name VARCHAR(20),
-    client_password VARCHAR(25),
-    team_id INT,
-    client_type VARCHAR(20)
-);
-CREATE TABLE Player_Agent (
-    player_agent_id SERIAL PRIMARY KEY,
+CREATE TABLE belongs_to (
+    id SERIAL PRIMARY KEY,
     player_id INT,
-    agent_id INT,
     team_id INT,
-    contract_date DATE,
+    start_date DATE,
+    uniform_number INT,
     contract_term INT,
     contract_payment INT
 );
-CREATE TABLE Team_Award (
-    team_award_id SERIAL PRIMARY KEY,
-    awards_id INT,
-    year INT,
+
+CREATE TABLE owner (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(63),
+    age INT,
+    budget INT
+);
+
+CREATE TABLE owns (
+    id SERIAL PRIMARY KEY,
+    owner_id INT,
     team_id INT
 );
-CREATE TABLE Individual_Award (
-    individual_award_id SERIAL PRIMARY KEY,
+
+CREATE TABLE director (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(25),
+    start_date DATE
+);
+
+CREATE TABLE directs (
+    id SERIAL PRIMARY KEY,
+    director_id INT,
+    team_id INT,
+    contract_term INT,
+    start_date DATE,
+    contract_payment INT
+);
+
+CREATE TABLE awards (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE player_won (
+    id SERIAL PRIMARY KEY,
+    player_id INT,
     awards_id INT,
-    year INT,
-    player_id INT
 );
-CREATE TABLE Awards (
-    awards_id SERIAL PRIMARY KEY,
-    awards_name VARCHAR(255)
+
+CREATE TABLE team_won (
+    id SERIAL PRIMARY KEY,
+    team_id INT,
+    awards_id INT,
 );
-CREATE TABLE Baseball_Records (
-    baseball_records_id SERIAL PRIMARY KEY,
-    team1_id INT,
-    team2_id INT,
-    baseball_stadium_id INT,
-    score VARCHAR(10)
-);
-CREATE TABLE Baseball_Stadium (
-    baseball_stadium_id SERIAL PRIMARY KEY,
-    location VARCHAR(255),
-    area INT,
-    capacity INT
+
+CREATE TABLE client (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    password INT,
+    team_id INT,
+    client_type VARCHAR(255)
+
 );
 
 
