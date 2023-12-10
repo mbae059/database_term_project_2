db88ae90c5eec7b2478050b132a505701ed3f015
mbae059 <mbae059@gmail.com>
2023-12-10 16:25:05 +0900

    changed team generator according to readme

--- create_dummy_data/Team.py
+++ create_dummy_data/Team.py
@@ -3,4 +3,4 @@ import random
 file = open('sql/team.sql', 'w')
 
 for i in range(1,11):
-    file.write(f"insert into team (team_name, owner_id, director_id, establishment_year) values ('team#{i}', {i}, {i}, {random.randint(1950, 2010)});\n")
+    file.write(f"insert into team (name, establishment_year) values ('team#{i}', {random.randint(1950, 1970)});\n")
